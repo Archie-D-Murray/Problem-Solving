ce082a5ee5dda1bcfc6bec63929cc75851ec5f6a
Archie-D-Murray <146730177+Archie-D-Murray@users.noreply.github.com>
2026-03-20 12:15:35 +0000

    feat: Week 8 labs tests

---
+++ Week08/Contest/robot.cpp
@@ -0,0 +1,248 @@
+#include <cstdint>
+#include <functional>
+#include <iostream>
+#include <queue>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+const char* bool_to_str(bool value) {
+    return value ? "True" : "False";
+}
+
+struct ivec2 {
+    int x;
+    int y;
+};
+
+struct obstacle_t {
+    int64_t value;
+    ivec2 position;
+};
+
+class obstacle_compare_t {
+public:
+    bool operator()(const obstacle_t& s, const obstacle_t& o) {
+        return s.value < o.value;
+    }
+};
+
+bool operator==(const ivec2& s, const ivec2& o) {
+    return (s.x == o.x) && (s.y == o.y);
+}
+
+template<> struct std::hash<ivec2> {
+    std::size_t operator() (const ivec2& vec) const noexcept {
+        return std::hash<int>{}(vec.x) ^ (std::hash<int>{}(vec.y) << 1);
+    }
+};
+
+enum fill_type { normal, inverted };
+
+void fill_grid(std::vector<int64_t>& grid, int nx, int ny, std::unordered_set<ivec2>& obstacle_positions, fill_type fill_mode, bool allow_obstacles = false) {
+    bool found_obstacle = false;
+    if (fill_mode == normal) {
+        for (int i = nx - 1; i >= 0; i--) {
+            if (obstacle_positions.count(ivec2{ i, ny - 1 }) && allow_obstacles) {
+                found_obstacle = true;
+                // std::cout << "Set Row:      [ " << i << ", " << ny - 1 << " ]\n";
+            }
+            grid[nx * (ny - 1) + i] = !found_obstacle;
+        }
+
+        found_obstacle = false;
+        for (int i = ny - 1; i >= 0; i--) {
+            if (obstacle_positions.count(ivec2{ nx - 1, i }) && allow_obstacles) {
+                // std::cout << "Set column:   [ " << nx - 1 << ", " << i << " ]\n";
+                found_obstacle = true;
+            }
+            grid[nx * i + nx - 1] = !found_obstacle;
+        }
+
+        for (int y = ny - 2; y >= 0; y--) {
+            for (int x = nx - 2; x >= 0; x--) {
+                if (!allow_obstacles || !obstacle_positions.count(ivec2{x, y})) {
+                    grid[y * nx + x] = grid[(y + 1) * nx + x] + grid[y * nx + x + 1];
+                } else {
+                    // std::cout << "Set obstacle: [ " << x << ", " << y << " ]\n";
+                    grid[y * nx + x] = 0;
+                }
+            }
+        }
+    } else {
+        for (int i = 0; i < nx; i++) {
+            if (obstacle_positions.count(ivec2{ i, 0 }) && allow_obstacles) {
+                found_obstacle = true;
+                // std::cout << "Set Row:      [ " << i << ", " << 0 << " ]\n";
+            }
+            grid[nx * 0 + i] = !found_obstacle;
+        }
+
+        found_obstacle = false;
+        for (int i = 0; i < ny; i++) {
+            if (obstacle_positions.count(ivec2{ 0, i }) && allow_obstacles) {
+                // std::cout << "Set column:   [ " << 0 << ", " << i << " ]\n";
+                found_obstacle = true;
+            }
+            grid[nx * i + 0] = !found_obstacle;
+        }
+
+        for (int y = 1; y < ny; y++) {
+            for (int x = 1; x < nx; x++) {
+                if (!allow_obstacles || !obstacle_positions.count(ivec2{x, y})) {
+                    grid[y * nx + x] = grid[(y - 1) * nx + x] + grid[y * nx + x - 1];
+                } else {
+                    // std::cout << "Set obstacle: [ " << x << ", " << y << " ]\n";
+                    grid[y * nx + x] = 0;
+                }
+            }
+        }
+    }
+}
+
+int64_t robot_paths(int nx, int ny, int k, std::vector<std::vector<int>>& obstacle_positions) {
+    std::vector<int64_t> grid = std::vector<int64_t>(nx * ny);
+    std::unordered_set<ivec2> obstacles = std::unordered_set<ivec2>(obstacle_positions.size());
+    for (std::vector<int>& pos : obstacle_positions) {
+        obstacles.insert(ivec2 { pos[0], pos[1] });
+    }
+    fill_grid(grid, nx, ny, obstacles, fill_type::normal);
+
+    // auto old = std::cout.flags();
+    // std::cout << std::scientific;
+    // std::cout.precision(2);
+    // std::cout << "\nGrid: \n";
+    // for (size_t y = 0; y < ny; y++) {
+    //     for (size_t x = 0; x < nx; x++) {
+    //         std::cout << (double) grid[y * nx + x] << ", ";
+    //     }
+    //     std::cout << "\n";
+    // }
+    // std::cout << "\n";
+
+
+    std::unordered_map<ivec2, int64_t> obstacle_map;
+    obstacle_map.reserve(obstacle_positions.size());
+    std::priority_queue<obstacle_t, std::vector<obstacle_t>, obstacle_compare_t> obstacle_heap;
+    for (ivec2 position : obstacles) {
+        obstacle_map[position] = grid[position.y * nx + position.x];
+    }
+
+    fill_grid(grid, nx, ny, obstacles, fill_type::inverted);
+    for (ivec2 position : obstacles) {
+        int64_t product = grid[position.y * nx + position.x];
+        obstacle_map[position] *= grid[position.y * nx + position.x];;
+    }
+
+    for (std::pair<ivec2, int64_t> pair : obstacle_map) {
+        // std::cout << "Calculated obstacle value at [ " << pair.first.x << ", " << pair.first.y << " ] - Value: " << pair.second << "\n";
+        obstacle_heap.push(obstacle_t { pair.second, pair.first });
+    }
+
+
+    obstacles.clear();
+    for (int i = 0; i < k; i++) {
+        // std::cout << "Removed obstacle at [ " << obstacle_heap.top().position.x << ", " << obstacle_heap.top().position.y << " ] - Value: " << obstacle_heap.top().value << "\n";
+        obstacle_heap.pop();
+    }
+
+    while (!obstacle_heap.empty()) {
+        obstacles.insert(obstacle_heap.top().position);
+        // std::cout << "Kept obstacle at [ " << obstacle_heap.top().position.x << ", " << obstacle_heap.top().position.y << " ] - Value: " << obstacle_heap.top().value << "\n";
+        obstacle_heap.pop();
+    }
+
+    fill_grid(grid, nx, ny, obstacles, fill_type::normal, true);
+    // std::cout << "\nObstacle Grid: \n";
+    // std::cout << std::scientific;
+    // std::cout.precision(2);
+    // for (size_t y = 0; y < ny; y++) {
+    //     for (size_t x = 0; x < nx; x++) {
+    //         std::cout << (double) grid[y * nx + x] << ", ";
+    //     }
+    //     std::cout << "\n";
+    // }
+    // std::cout << "\n";
+
+    return grid[0];
+}
+
+struct robot_test_t {
+    int nx;
+    int ny;
+    int k;
+    std::vector<std::vector<int>> obstacle_positions;
+    int64_t expected;
+};
+
+#define torture_test
+
+void test_robot_paths() {
+    std::vector<robot_test_t> tests = {
+        #ifndef torture_test
+        robot_test_t {
+            .nx = 3,
+            .ny = 2,
+            .k = 1,
+            .obstacle_positions = {
+                {1, 1}
+            },
+            .expected = 3
+        },
+        robot_test_t {
+            .nx = 6,
+            .ny = 6,
+            .k = 2,
+            .obstacle_positions = {
+                { 0, 2 },
+                { 4, 4 },
+                { 5, 2 },
+                { 5, 4 },
+            },
+            .expected = 176
+        },
+        #else
+        robot_test_t {                  //  20 10 8 4
+            .nx = 1000,                   // 1 0
+            .ny = 1000,                   // 1 3
+            .k = 5,                     // 5 0
+            .obstacle_positions = {     // 9 7
+                { 595, 418 },
+                { 513, 575 },
+                { 766, 492 },
+                { 780, 714 },
+                { 811, 215 },
+                { 636, 284 },
+                { 590, 480 },
+                { 752, 728 },
+                { 569, 475 },
+                { 620, 523 },
+                { 805, 689 },
+                { 585, 779 },
+                { 601, 713 },
+                { 716, 468 },
+                { 824, 425 },
+                { 510, 579 },
+                { 501, 554 },
+                { 551, 460 },
+                { 914, 506 },
+                { 646, 515 },
+            },
+            .expected = 5819285640465217080l
+        },
+        #endif
+    };
+
+    for (robot_test_t& test : tests) {
+        std::cout << "\n---------------------------\n";
+        int64_t output = robot_paths(test.nx, test.ny, test.k, test.obstacle_positions);
+
+        std::cout << "Output:   " << output << "\n";
+        std::cout << "Expected: " << test.expected << "\n";
+        std::cout << "Match:    " << bool_to_str(output == test.expected) << "\n";
+        std::cout << "---------------------------\n";
+    }
+}
+
+int main(void) {
+    test_robot_paths();
+}
---
+++ Week08/Lab/main.cpp
@@ -0,0 +1,193 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+const char* bool_to_str(bool value) {
+    return value ? "True" : "False";
+}
+
+template<typename T> void print_vector(std::vector<T> vector, bool new_line = true) {
+    std::cout << "[ ";
+    size_t i = 0;
+    for (size_t i = 0; i < vector.size(); i++) {
+        if (i < vector.size() - 1) {
+            std::cout << vector[i] << ", ";
+        } else {
+            std::cout << vector[i];
+        }
+    }
+    if (new_line) {
+        std::cout << " ] \n";
+    } else {
+        std::cout << " ]";
+    }
+}
+
+std::vector<std::vector<int>> pascal_triangle(int rows) {
+    return {};
+}
+
+struct pascal_test_t {
+    std::vector<std::vector<int>> expected;
+    int rows;
+};
+
+void test_pascal_triangle(void) {
+    std::vector<pascal_test_t> tests = {
+        pascal_test_t {
+            .expected = { 
+                {       1,        },
+                {      1, 1,      },
+                {    1, 2, 1,     },
+                {   1, 3, 3, 1,   },
+                { 1, 4, 6, 4, 1,  },
+            },
+            .rows = 5
+        },
+    };
+
+    for (pascal_test_t& test : tests) {
+        std::cout << "\n-------------------------------\n";
+        std::vector<std::vector<int>> output = pascal_triangle(test.rows);
+        std::cout << "Input: " << test.rows << "\n";
+        bool match = output.size() == test.expected.size();
+
+        for (size_t i = 0; i < test.expected.size() && match; i++) {
+            match = output[i] == test.expected[i];
+        }
+
+        std::cout << "Output:\n";
+        for (size_t i = 0; i < test.rows; i++) {
+            for (size_t pad = 0; pad < test.rows - i; pad++) {
+                std::cout << " ";
+            }
+
+            for (size_t j = 0; j < output[i].size(); j++) {
+                std::cout << output[i][j] << " ";
+            }
+            std::cout << "\n";
+        }
+
+        std::cout << "Expected:\n";
+        for (size_t i = 0; i < test.rows; i++) {
+            for (size_t pad = 0; pad < test.rows - i; pad++) {
+                std::cout << " ";
+            }
+
+            for (size_t j = 0; j < output[i].size(); j++) {
+                std::cout << output[i][j] << " ";
+            }
+            std::cout << "\n";
+        }
+
+        std::cout << "Match: " << bool_to_str(match) << "\n";
+        std::cout << "-------------------------------\n";
+    }
+}
+
+std::vector<std::string> letter_combos(std::string& digits) {
+    return {};
+}
+
+struct letter_combo_test_t {
+    std::string input;
+    std::vector<std::string> expected;
+};
+
+void test_letter_combo(void) {
+    std::vector<letter_combo_test_t> tests = {
+        letter_combo_test_t {
+            .input = "23",
+            .expected = {"ad","ae","af","bd","be","bf","cd","ce","cf"}
+        },
+        letter_combo_test_t {
+            .input = "2",
+            .expected = {"a","b","c"}
+        },
+    };
+
+    for (letter_combo_test_t& test : tests) {
+        std::cout << "\n-------------------------------\n";
+        std::vector<std::string> output = letter_combos(test.input);
+        std::cout << "Input:    " << test.input << "\n";
+        std::cout << "Output:   ";
+        print_vector(output);
+        std::cout << "Expected: ";
+        print_vector(test.expected);
+        std::cout << "Match: " << bool_to_str(output == test.expected) << "\n";
+        std::cout << "-------------------------------\n";
+    }
+}
+
+int num_decodings(std::string& s) {
+    return {};
+}
+
+struct num_decodings_test_t {
+    std::string input;
+    int expected;
+};
+
+void test_num_decodings(void) {
+    std::vector<num_decodings_test_t> tests = {
+        num_decodings_test_t {
+            .input = "12",
+            .expected = 2
+        },
+        num_decodings_test_t {
+            .input = "226",
+            .expected = 3
+        },
+    };
+
+    for (num_decodings_test_t& test : tests) {
+        std::cout << "\n-------------------------------\n";
+        int output = num_decodings(test.input);
+        std::cout << "Input:    " << test.input << "\n";
+        std::cout << "Output:   " << output << "\n";
+        std::cout << "Expected: " << test.expected << "\n";
+        std::cout << "Match: " << bool_to_str(output == test.expected) << "\n";
+        std::cout << "-------------------------------\n";
+    }
+}
+
+int coin_change(int n, std::vector<int>& c) {
+    return {};
+}
+
+struct coin_change_test_t {
+    int amount;
+    std::vector<int> denominations;
+    int expected;
+};
+
+void test_coin_change(void) {
+    std::vector<coin_change_test_t> tests = {
+        coin_change_test_t {
+            .amount = 4,
+            .denominations = { 1, 2, 3 },
+            .expected = 4,
+        },
+        coin_change_test_t {
+            .amount = 10,
+            .denominations = { 2, 5, 3, 6 },
+            .expected = 5,
+        },
+    };
+
+    for (coin_change_test_t& test : tests) {
+        std::cout << "\n-------------------------------\n";
+        int output = coin_change(test.amount, test.denominations);
+        std::cout << "Input:    " << test.amount << ", ";
+        std::cout << "Denominations: ";
+        print_vector(test.denominations);
+        std::cout << "Output:   " << output << "\n";
+        std::cout << "Expected: " << test.expected << "\n";
+        std::cout << "Match:    " << bool_to_str(output == test.expected) << "\n";
+        std::cout << "-------------------------------\n";
+    }
+}
+
+int main(void) {
+
+}
